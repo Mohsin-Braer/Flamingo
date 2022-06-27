a9d873774dd1866ad72a2bf308267d2a7250a636
Mohsin Braer <hbraer@gmail.com>
2022-06-26 23:46:18 -0400

    Setup Home news feed UI component

--- components/side-bar/side-bar-container/side-bar.component.js
+++ components/side-bar/side-bar-container/side-bar.component.js
@@ -15,12 +15,11 @@ import {
 
 const SideBar = () => {
     return(
-        <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 h-full">
+        <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
             <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
                 <Image src="https://i.postimg.cc/25Qx5dSQ/flamingo-pink.png" width={30} height={30}/>
             </div>
             <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
-                {/* <HomeIcon className="h-7" /> */}
                 <SidebarLink text="Home" Icon={HomeIcon} active />
                 <SidebarLink text="Explore" Icon={HashtagIcon} />
                 <SidebarLink text="Notifications" Icon={BellIcon} />
@@ -30,7 +29,7 @@ const SideBar = () => {
                 <SidebarLink text="Profile" Icon={UserIcon} />
                 <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
             </div>
-            <button className="hidden xl:inline ml-auto bg-[#e88484] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#ea6464]">
+            <button className="hidden xl:inline ml-auto bg-[#e88484] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#ea6464] mt-[20px]">
                     Tweet
             </button>
 
@@ -38,7 +37,7 @@ const SideBar = () => {
                 <img src="https://lh3.googleusercontent.com/a/AATXAJwCsuneWAkKlHwMPxOmLNjFACEvbtN8QPwbUsZ-=s96-c" alt="" 
                           className="h-10 w-10 rounded-full xl:m-r-2.5" /> {/** Using base img rather than Image comp because not sure where img coming from */}
 
-                <div className="hidden xl:inline leading-5">
+                <div className="hidden xl:inline leading-5 ml-[4px] ">
                     <h4 className="font-bold">Testing</h4>
                     <p className="text-[#6e767d]">@test</p>
                 </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,24 +3,21 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import SideBar from '../components/side-bar/side-bar-container/side-bar.component';
-import Feed from '../components/news-feed/news-feed.components';
+import NewsFeed from '../components/news-feed/news-feed.components';
 
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
-        <title> Flamingo </title>
+        <title>Home / Flamingo </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     
 
-      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto"> {/**custom color: bg-[]*/}
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
         <SideBar />
-        <Feed />
-        {/* Widgets */}
-
-        {/* Modal */}
+        <NewsFeed />
 
       </main>
     </div>
